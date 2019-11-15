0bcb5cac2d6b9479718a1002270dff21c9109e8f
keddad <keddad@yandex.ru>
2019-11-15 23:00:08 +0300

    Now using flake8

--- owo/api/testing.py
+++ owo/api/testing.py
@@ -1,11 +1,9 @@
 from pymongo import MongoClient
 from loguru import logger
 from flask import Blueprint
-from owo.api.utils import schema_validator
-from owo.api.schemas import *
 
 
-logger.add("api_admin.log", colorize=True,
+logger.add("api_testing.log", colorize=True,
            format="<green>{time}</green> <level>{message}</level>", rotation="1 day", backtrace=True, diagnose=True)
 
 client = MongoClient('mongodb://mongo:27017/')
