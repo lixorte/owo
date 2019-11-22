715e25cd04ad5f3dd245d2ee026b4a84c41f09c7
keddad <keddad@yandex.ru>
2019-11-22 20:48:53 +0300

    Implemented /user get

--- owo/api/user.py
+++ owo/api/user.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from loguru import logger
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
 from owo.api.utils import schema_validator, validate_ej
 from owo.security.hashing import hash_new_password
 import owo.api.schemas as schemas
@@ -59,3 +59,35 @@ def add_user():
     return Response(
         status=200
     )
+
+
+@user.route("/user", methods=["get"])
+@schema_validator(schemas.get_users)
+def get_users():
+    data = request.get_json()
+
+    offset = data.get("offset", None) or 0
+    limit = data.get("limit")
+    utype = data.get("type")
+
+    out = list()
+
+    for idx, item in enumerate(
+        client["meta"]["users"].find().sort("login")
+    ):
+        if idx < offset:
+            continue
+        if idx > offset + limit:
+            break
+        if ((utype == "admin" and data["state"] != "admin")
+                or (utype == "normal" and data["state"] != "normal")):
+            continue
+        else:
+            del item["hpassword"]
+            del item["hejpassword"]
+            item["id"] = str(item["_id"])
+            del item["_id"]
+            
+            out.append(item)
+
+    return (jsonify(out), 200)
