f4ff358c7b8c68fdd6155102d2ca7dbe49c725fd
lixorte <evelinabrugger@gmail.com>
2019-12-22 20:44:39 +0300

    Implemented "add song" form

--- owo/static/index.js
+++ owo/static/index.js
@@ -7,3 +7,26 @@ function toggleSidebar() {
         document.getElementById("sb-btn-head").style.display = "block";
     }
 }
+
+function sendData() {
+    if (document.getElementById("input-song-title").checkValidity() === false ||
+        document.getElementById("input-song-author").checkValidity() === false) {
+        document.getElementById("required-alert").style.display = "block";
+        return;
+    } else {
+        document.getElementById("required-alert").style.display = "none";
+    }
+
+    let electionUid = "";
+    let cookies = document.cookie;
+    let jwt = cookies["JWT"];
+    let addItemData = JSON.stringify($(".add-song").serializeArray());
+
+    XMLHttpRequest.open("POST", "/election/" + electionUid + "/vote/new");
+    XMLHttpRequest.setRequestHeader("Authorization", "Bearer " + jwt);
+    XMLHttpRequest.send(addItemData);
+}
+
+function f() {
+
+}
