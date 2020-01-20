55bc229c9d41d8afb85bbba69942cb470721a31c
keddad <keddad@yandex.ru>
2020-01-20 12:29:56 +0300

    Added some features to test JWT

--- owo/api/testing.py
+++ owo/api/testing.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from loguru import logger
+from .utils import prefs_validator
 from flask import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
@@ -13,3 +14,17 @@ tesing_blueprint = Blueprint('testing', __name__)
 @jwt_required
 def get_jwt():
     return get_jwt_identity()
+
+
+@tesing_blueprint.route("/only/admin")
+@jwt_required
+@prefs_validator({"type": "admin"})
+def only_admin():
+    return "Hello admin"
+
+
+@tesing_blueprint.route("/only/banned")
+@jwt_required
+@prefs_validator({"state": "banned"})
+def only_banned():
+    return "Hello looser"
